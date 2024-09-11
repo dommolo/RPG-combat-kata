65d84550eabc1115fdd5bc42b40d2b198b12240b
Dom <dom@nurturepad.it>
2024-09-11 21:40:08 +0200

    REFACTORING: improve tests

--- src/main.js
+++ src/main.js
@@ -1,45 +1,47 @@
+var maxHealth = 1000;
+
 function Character() {
-  this.health = 1000;
+  this.health = maxHealth;
   this.level = 1;
   this.alive = true;
   this.damageAmount = 1;
   this.healAmount = 1;
-  
-  this.attack = function(c) {
+
+  this.attack = function (c) {
     if (c == this)
       return;
-    
+
     c.attackFrom(this);
   };
-  
-  this.attackFrom = function(c) {
+
+  this.attackFrom = function (c) {
     if (c == this)
       return;
-    
+
     var realDamageAmount = c.damageAmount;
     if (this.level >= c.level + 5)
       realDamageAmount *= .5;
     else if (this.level <= c.level - 5)
       realDamageAmount *= 1.5;
-    
+
     if (realDamageAmount >= this.health) {
       this.health = 0;
       this.alive = false;
     } else
       this.health -= realDamageAmount;
   };
-  
-  this.heal = function(c) {
+
+  this.heal = function (c) {
     c.healFrom(this);
   };
-  
-  this.healFrom = function(c) {
+
+  this.healFrom = function (c) {
     if (!this.alive)
       return;
-    
+
     this.health += c.healAmount;
-    if (this.health > 1000)
-      this.health = 1000;
+    if (this.health > maxHealth)
+      this.health = maxHealth;
   };
 }
 
--- test/test.js
+++ test/test.js
@@ -13,46 +13,106 @@ function testEquals(actual, expected, message) {
     console.log('Expected value: ' + expected + ', actual value: ' + actual);
 }
 
-c = new rpg.Character();
+var tests = [];
 
-test(c, 'Characters can be created');
-testEquals(c.health, 1000, 'New characters have 1000 Health');
-testEquals(c.level, 1, 'New characters start from level 1');
-test(c.alive !== undefined, 'Characters can be Alive or Dead');
-test(c.alive, 'New characters are Alive');
-test(typeof c.attack == 'function', 'Characters can Deal Damage to Characters');
+tests.push(function () {
+  var c = new rpg.Character();
 
-c2 = new rpg.Character();
-c.attack(c2);
+  test(c, 'Characters can be created');
+});
 
-test(c2.health < 1000, 'Damage is subtracted from Health');
+tests.push(function () {
+  var c = new rpg.Character();
 
-for (var i=0; i<1000; i++)
-  c.attack(c2);
+  testEquals(c.health, 1000, 'New characters have 1000 Health');
+});
+tests.push(function () {
+  var c = new rpg.Character();
 
-testEquals(c2.health, 0, 'When damage received exceeds current Health, Health becomes 0');
-test(!c2.alive, 'and the character is now Dead');
+  testEquals(c.level, 1, 'New characters start from level 1');
+});
 
-c3 = new rpg.Character();
-c.attack(c3);
-c.heal(c3);
+tests.push(function () {
+  var c = new rpg.Character();
 
-testEquals(c3.health, 1000, 'A Character can Heal a Character');
+  test(c.alive !== undefined, 'Characters can be Alive or Dead');
+});
 
-c.heal(c2);
-testEquals(c2.health, 0, 'Dead characters cannot be healed');
+tests.push(function () {
+  var c = new rpg.Character();
 
-c3.heal(c);
-testEquals(c.health, 1000, 'Healing cannot raise health above 1000');
+  test(c.alive, 'New characters are Alive');
+});
 
-c.attack(c);
-testEquals(c.health, 1000, 'A Character cannot Deal Damage to itself');
+tests.push(function () {
+  var c = new rpg.Character();
 
-c4 = new rpg.Character();
-c4.level = 6;
-c.attack(c4);
-testEquals(c4.health, 1000 - c.damageAmount * .5, 'If the target is 5 or more Levels above the attacker, Damage is reduced by 50%');
+  test(typeof c.attack == 'function', 'Characters can Deal Damage to Characters');
+});
 
-c5 = new rpg.Character();
-c4.attack(c5);
-testEquals(c5.health, 1000 - c4.damageAmount * 1.5, 'If the target is 5 or more Levels below the attacker, Damage is increased by 50%');
+tests.push(function () {
+  var c1 = new rpg.Character(), c2 = new rpg.Character();
+  c1.attack(c2);
+
+  test(c2.health < 1000, 'Damage is subtracted from Health');
+});
+
+tests.push(function () {
+  var c1 = new rpg.Character(), c2 = new rpg.Character();
+  for (var i = 0; i < 1000; i++)
+    c1.attack(c2);
+
+  testEquals(c2.health, 0, 'When damage received exceeds current Health, Health becomes 0');
+  test(!c2.alive, 'and the character is now Dead');
+});
+
+tests.push(function () {
+  var c1 = new rpg.Character(), c2 = new rpg.Character();
+  c2.health = 999;
+  c1.heal(c2);
+
+  testEquals(c2.health, 1000, 'A Character can Heal a Character');
+});
+
+tests.push(function () {
+  var c1 = new rpg.Character(), c2 = new rpg.Character();
+  c2.health = 0;
+  c2.alive = false;
+  c1.heal(c2);
+
+  testEquals(c2.health, 0, 'Dead characters cannot be healed');
+});
+
+tests.push(function () {
+  var c1 = new rpg.Character(), c2 = new rpg.Character();
+  c2.heal(c1);
+
+  testEquals(c1.health, 1000, 'Healing cannot raise health above 1000');
+});
+
+tests.push(function () {
+  var c = new rpg.Character();
+  c.attack(c);
+
+  testEquals(c.health, 1000, 'A Character cannot Deal Damage to itself');
+});
+
+tests.push(function () {
+  var c1 = new rpg.Character(), c2 = new rpg.Character();
+
+  c2.level = 6;
+  c1.attack(c2);
+
+  testEquals(c2.health, 1000 - c1.damageAmount * .5, 'If the target is 5 or more Levels above the attacker, Damage is reduced by 50%');
+});
+
+tests.push(function () {
+  var c1 = new rpg.Character(), c2 = new rpg.Character();
+  c2.level = 6;
+  c2.attack(c1);
+
+  testEquals(c1.health, 1000 - c2.damageAmount * 1.5, 'If the target is 5 or more Levels below the attacker, Damage is increased by 50%');
+});
+
+for (var i in tests)
+  tests[i]();
