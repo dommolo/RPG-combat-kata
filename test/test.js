02656d11736207611637ce1bdc9a0b8235425633
Dom <dom@nurturepad.it>
2024-09-11 17:43:20 +0200

    REFACTORING: add new test function: testEquals

--- test/test.js
+++ test/test.js
@@ -5,11 +5,19 @@ function test(item, message) {
   console.assert(item);
 }
 
+function testEquals(actual, expected, message) {
+  var out = expected == actual;
+  console.log('- ' + message);
+  console.assert(out);
+  if (!out)
+    console.log('Expected value: ' + expected + ', actual value: ' + actual);
+}
+
 c = new rpg.Character();
 
 test(c, 'Characters can be created');
-test(c.health == 1000, 'New characters have 1000 Health');
-test(c.level == 1, 'New characters start from level 1');
+testEquals(c.health, 1000, 'New characters have 1000 Health');
+testEquals(c.level, 1, 'New characters start from level 1');
 test(c.alive !== undefined, 'Characters can be Alive or Dead');
 test(c.alive, 'New characters are Alive');
 test(typeof c.attack == 'function', 'Characters can Deal Damage to Characters');
@@ -22,22 +30,22 @@ test(c2.health < 1000, 'Damage is subtracted from Health');
 for (var i=0; i<1000; i++)
   c.attack(c2);
 
-test(c2.health == 0, 'When damage received exceeds current Health, Health becomes 0');
+testEquals(c2.health, 0, 'When damage received exceeds current Health, Health becomes 0');
 test(!c2.alive, 'and the character is now Dead');
 
 c3 = new rpg.Character();
 c.attack(c3);
 c.heal(c3);
 
-test(c3.health == 1000, 'A Character can Heal a Character');
+testEquals(c3.health, 1000, 'A Character can Heal a Character');
 
 c.heal(c2);
-test(c2.health == 0, 'Dead characters cannot be healed');
+testEquals(c2.health, 0, 'Dead characters cannot be healed');
 
 c.attack(c);
-test(c.health == 1000, 'A Character cannot Deal Damage to itself');
+testEquals(c.health, 1000, 'A Character cannot Deal Damage to itself');
 
 c4 = new rpg.Character();
 c4.level = 6;
 c.attack(c4);
-test(c4.health == 1000 - c.damageAmount * .5, 'If the target is 5 or more Levels above the attacker, Damage is reduced by 50%');
+testEquals(c4.health, 1000 - c.damageAmount * .5, 'If the target is 5 or more Levels above the attacker, Damage is reduced by 50%');
